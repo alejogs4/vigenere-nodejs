0654b3317f61253e06d781a5a88c777632c75088
alejogs4 <alejogs4@gmail.com>
2019-10-05 17:37:51 -0500

    Update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   // eslint-disable-next-line no-console
-  console.log('Application running http://localhost:3000');
+  console.log('Application running http://localhost:3002');
 });
